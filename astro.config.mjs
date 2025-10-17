212fe35a2c467862206af537e807ebbf1d37969e
alejandromacedop <macedoalejandro12@gmail.com>
2025-10-16 19:33:43 -0500

    uploading v.1.2 deploy prod

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel/static'
 
 export default defineConfig({
+  output: 'static',
   vite: { plugins: [tailwind()] },
+  adapter: vercel(),
 });
